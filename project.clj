48f0846aa91043b71b681fc2fa15ae4e459687fd
Alex Chin <achin@42six.com>
2014-01-15 15:49:40 -0500

    Fix project name.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deploy-artifacts "0.1.0"
+(defproject lein-deploy-artifacts "0.1.0"
   :description "Deploy artifacts to remote repositories"
   :url "https://github.com/achin/lein-deploy-artifact"
   :license {:name "Eclipse Public License"
