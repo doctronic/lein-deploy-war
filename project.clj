fc71b44e216db619a1e4d6a8eb276229869b9603
Alex Chin <achin@42six.com>
2014-01-15 15:52:44 -0500

    Begin 0.2.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-deploy-artifacts "0.1.0"
+(defproject lein-deploy-artifacts "0.2.0-SNAPSHOT"
   :description "Deploy artifacts to remote repositories"
   :url "https://github.com/achin/lein-deploy-artifact"
   :license {:name "Eclipse Public License"
