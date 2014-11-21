92d06e6788f23259643476e35e8b0d34002e02da
Greg Woodfill <greg.woodfill@bestbuy.com>
2014-11-21 14:53:36 -0600

    initial changes from deploy artifacts

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject lein-deploy-artifacts "0.2.0-SNAPSHOT"
-  :description "Deploy artifacts to remote repositories"
+(defproject lein-deploy-war "0.1.0-SNAPSHOT"
+  :description "Deploy war files to remote repositories"
   :url "https://github.com/achin/lein-deploy-artifact"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
