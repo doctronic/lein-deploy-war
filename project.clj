1fe6d689232064d340b83f3553df6cfbcf47205e
Greg Woodfill <greg@gregwoodfill.com>
2014-11-21 22:08:44 -0600

    Adding scm info
    Signed-off-by: Greg Woodfill <greg@gregwoodfill.com>

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/gregwoodfill/lein-deploy-war"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/gregwoodfill/lein-deploy-war"}
   :eval-in-leiningen true)
