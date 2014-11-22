0257c733a61af0a28ec37724481d9e775c862ee7
Greg Woodfill <greg@gregwoodfill.com>
2014-11-21 18:49:36 -0600

    updating group for clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-deploy-war "0.1.0-SNAPSHOT"
+(defproject org.clojars.gregwoodfill/lein-deploy-war "0.1.0-SNAPSHOT"
   :description "Deploy war files to remote repositories"
-  :url "https://github.com/achin/lein-deploy-artifact"
+  :url "https://github.com/gregwoodfill/lein-deploy-war"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
