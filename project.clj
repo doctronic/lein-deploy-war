d34ca842a01951b9ab95e590943ecbbc8f0beb5a
hemmvm <hemmvm@gmail.com>
2016-04-08 14:05:08 +0200

    Bump version "0.2.1"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.doctronic/lein-deploy-war "0.2.1-SNAPSHOT"
+(defproject de.doctronic/lein-deploy-war "0.2.1"
   :description "Deploy war files to remote repositories. Should be used in conjunction with `ring uberwar`"
   :url "https://github.com/doctronic/lein-deploy-war"
   :license {:name "Eclipse Public License"
