dc16760db26949f9925c3ab5a729885bcc9689d6
hemmvm <hemmvm@gmail.com>
2016-04-08 13:57:56 +0200

    Update SCM info

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject de.doctronic/lein-deploy-war "0.2.1-SNAPSHOT"
-  :description "Deploy war files to remote repositories. Should be used in conjunction with `ring war`"
-  :url "https://github.com/gregwoodfill/lein-deploy-war"
+  :description "Deploy war files to remote repositories. Should be used in conjunction with `ring uberwar`"
+  :url "https://github.com/doctronic/lein-deploy-war"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
-        :url "https://github.com/gregwoodfill/lein-deploy-war"}
+        :url "https://github.com/doctronic/lein-deploy-war"}
   :eval-in-leiningen true)
